0c0134e16a36e9ae2695f2ffdeeb90f0bfc49257
khadada <lkhadada@gmail.com>
2022-04-20 23:48:27 +0100

    I Imported necessary modules and classes

---
+++ main.py
@@ -0,0 +1,4 @@
+# Importinng necessary modules and classes:
+import sys
+from PyQt5.QtWidgets import (QMainWindow,QApplication,QLabel,QComboBox,QLineEdit,QTextEdit,QWidget,QToolBar,QStatusBar)
+from PyQt5.QtGui import (QPalette,QColor,QFont)
