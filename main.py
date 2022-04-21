45f070373e6fcec72117952109108729ddbf7fdf
khadada <lkhadada@gmail.com>
2022-04-21 01:01:09 +0100

    Start working with the main class app

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 import sys
 from PyQt5.QtWidgets import (QMainWindow,QApplication,QLabel,QComboBox,QLineEdit,QTextEdit,QWidget,QToolBar,QStatusBar)
 from PyQt5.QtGui import (QPalette,QColor,QFont)
+
+# MainWindowApp class
+class PermanonceApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initialize_ui()
+        self.show()
+    
