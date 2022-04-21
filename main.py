de4fe9d622ccc2e6b76f3ca5012ffce69677563c
khadada <lkhadada@gmail.com>
2022-04-21 15:14:29 +0100

    I import other classes and keep track the other window by adding workspace attribute , I start create layouts & widgets for the program

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # Importinng necessary modules and classes:
 import sys
-from PyQt5.QtWidgets import (QMainWindow,QApplication,QLabel,QComboBox,QLineEdit,QTextEdit,QWidget,QToolBar,QStatusBar)
-from PyQt5.QtGui import (QPalette,QColor,QFont)
+from PyQt5.QtWidgets import (QMainWindow,QApplication,QLabel,QComboBox,QLineEdit,QTextEdit,QWidget,QToolBar,QStatusBar,QGridLayout,QPushButton)
+from PyQt5.QtGui import (QPalette,QColor,QFont,QIcon)
 
 # MainWindowApp class
 class PermanonceApp(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.workspace = None
         self.initialize_ui()
         self.show()
     
+    def initialize_ui(self):
+        """
+        Initialize the main window and display its content
+        """
+        self.setGeometry(10, 10, 800, 600)
+        self.setWindowTitle("برنامج الخفير فرقة تماسين")
+        self.setWindowIcon(QIcon("icons/policeman.png"))
+        self.display_content()
+    
+    def display_content(self):
+        """
+        Display the content of the main window
+        """
+        # Create vertical layout
+        main_layout = QGridLayout()
+        self.day_event_btn = QPushButton
+        
+        
